3845fb673ee9a40b84f4b0c6fd33159a576ef42b
Nicolas Dupont <dupontnicolas703@gmail.com>
2025-12-31 02:43:44 +0100

    Added data models

---
+++ Data/Composition.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data;
+
+public class Composition
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+    public virtual ICollection<Role> Roles { get; set; } = new List<Role>();
+    
+    public override string ToString() => $"{Name} : {Description}" + Roles.Select(role => $"- {role}\n");
+}
---
+++ Data/LgDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class LgDbContext(DbContextOptions<LgDbContext> options) : DbContext(options)
+{
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Player> Players { get; set; }
+    public DbSet<Composition> Compositions { get; set; }
+    public DbSet<Group> Groups { get; set; }
+}
