4b868262488631c0410fe5d04382ae6fd4fe5772
Nicolas Dupont <dupontnicolas703@gmail.com>
2026-01-02 03:51:02 +0100

    Using nameof instead of defined code

--- Logic/LG/ActionType.cs
+++ Logic/LG/ActionType.cs
@@ -2,23 +2,7 @@
 
 public enum ActionType
 {
-    Next
-}
-
-public static class ActionExtensions
-{
-    extension(ActionType actionType)
-    {
-        public string ToDescription() => actionType switch
-            {
-                ActionType.Next => "Next step",
-                _ => "Unknown"
-            };
-
-        public string ToCode() => actionType switch
-            {
-                ActionType.Next => "NEXT",
-                _ => "UNKNOWN"
-            };
-    }
+    Next,
+    LgChoice,
+    Vote
 }
